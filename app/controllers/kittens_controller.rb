fd9dac21ec5e53c19b6c6e0188cc1e53427dae07
jfonz412 <jfonzone@outlook.com>
2017-06-22 15:58:36 -0400

    new and edit kittens

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,8 +1,15 @@
 class KittensController < ApplicationController
 	def new
+		@kitten = Kitten.new
 	end
 
 	def create
+		@kitten = Kitten.new(kitten_params)
+		if @kitten.save
+			redirect_to root_url
+		else
+			render 'new'
+		end
 	end
 
 	def index
@@ -12,11 +19,23 @@ class KittensController < ApplicationController
 	end
 
 	def edit
+		@kitten = Kitten.find(params[:id])
 	end
 
 	def update
+		@kitten = Kitten.find(params[:id])
+		if @kitten.update_attributes(kitten_params)
+			redirect_to root_url
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
 	end
+
+	private
+		def kitten_params
+			params.require(:kitten).permit(:name,:age,:cuteness,:softness)
+		end
 end
