e325b654d92d9ec4056ad5af40504fcb4f4e28de
jfonz412 <jfonzone@outlook.com>
2017-06-22 15:34:58 -0400

    empty controller actions and views for all Kitten restful actions

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,22 @@
 class KittensController < ApplicationController
+	def new
+	end
+
+	def create
+	end
+
+	def index
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
 end
